c4c5ab25848bd7f8b149b373fa53b48a9ff225e9
Mafioziks <tt00792@gmail.com>
2017-04-03 22:07:45 +0300

    Add more data

--- EstateSearchWidget.php
+++ EstateSearchWidget.php
@@ -16,28 +16,14 @@ class EstateSearchWidget extends WP_Widget {
 		// Check values
 		if( $instance) {
 			$title = esc_attr($instance['title']);
-			$text = esc_attr($instance['text']);
-			$textarea = esc_textarea($instance['textarea']);
 		} else {
 			$title = '';
-			$text = '';
-			$textarea = '';
 		}
 		?>
 		
 		<p>
 		<label for="<?php echo $this->get_field_id('title'); ?>"><?php _e('Widget Title', 'wp_widget_plugin'); ?></label>
 		<input class="widefat" id="<?php echo $this->get_field_id('title'); ?>" name="<?php echo $this->get_field_name('title'); ?>" type="text" value="<?php echo $title; ?>" />
-		</p>
-		
-		<p>
-		<label for="<?php echo $this->get_field_id('text'); ?>"><?php _e('Text:', 'wp_widget_plugin'); ?></label>
-		<input class="widefat" id="<?php echo $this->get_field_id('text'); ?>" name="<?php echo $this->get_field_name('text'); ?>" type="text" value="<?php echo $text; ?>" />
-		</p>
-		
-		<p>
-		<label for="<?php echo $this->get_field_id('textarea'); ?>"><?php _e('Textarea:', 'wp_widget_plugin'); ?></label>
-		<textarea class="widefat" id="<?php echo $this->get_field_id('textarea'); ?>" name="<?php echo $this->get_field_name('textarea'); ?>"><?php echo $textarea; ?></textarea>
 		</p>
 		
 		<?php
@@ -52,8 +38,6 @@ class EstateSearchWidget extends WP_Widget {
 		$instance = $old_instance;
 		// Fields
 		$instance['title'] = strip_tags($new_instance['title']);
-		$instance['text'] = strip_tags($new_instance['text']);
-		$instance['textarea'] = strip_tags($new_instance['textarea']);
 		return $instance;
 	}
 	
@@ -68,26 +52,64 @@ class EstateSearchWidget extends WP_Widget {
 		extract( $args );
 		// these are the widget options
 		$title = apply_filters('widget_title', $instance['title']);
-		$text = $instance['text'];
-		$textarea = $instance['textarea'];
 		echo $before_widget;
 		// Display the widget
 		echo '<div class="widget-text wp_widget_plugin_box">';
 		
 		// Check if title is set
-		if ( $title ) {
-			echo $before_title . $title . $after_title;
-		}
+// 		if ( $title ) {
+// 			echo $before_title . $title . $after_title;
+// 		}
 		
+		// Get estate types
 		$estateType = get_terms( array(
 		    'taxonomy' => 'estate_type',
 		    'hide_empty' => false,
 		) );
+		$types = [];
+		foreach ($estateType as $type) {
+			$types[] = $type->name;
+		}
 		
+		// Get estate series
 		$estateSerie = get_terms( array(
 				'taxonomy' => 'estate_serie',
 				'hide_empty' => false
 		) );
+		$series = [];
+		foreach ($estateSerie as $serie) {
+			$series[] = $serie->name;
+		}
+		
+		// Get estate actions
+		$estateActions = get_terms( array(
+		'taxonomy' => 'action',
+		'hide_empty' => false
+		) );
+		$actions = [];
+		foreach ($estateActions as $action) {
+			$actions[] = $action->name;
+		}
+		
+		// Get estate cities
+		$estateRegions = get_terms( array(
+		'taxonomy' => 'region',
+		'hide_empty' => false
+		) );
+		$regions = [];
+		foreach ($estateRegions as $region) {
+			$regions[] = $region->name;
+		}
+		
+		// Get estate residentals
+		$estateResidentals = get_terms( array(
+		'taxonomy' => 'residental',
+		'hide_empty' => false
+		) );
+		$residentals = [];
+		foreach ($estateResidentals as $residental) {
+			$residentals[] = $residental->name;
+		}
 		
 		?>
 		<form id="estate-search" role="search" method="get" id="searchform" action="/">
@@ -95,88 +117,164 @@ class EstateSearchWidget extends WP_Widget {
 			<div>
 				<h2><?php echo $text; ?></h2>
 				<div>
-					<label for="s"><?php echo __('Meklēt: ', 'textdomain'); ?></label>
-					<input type="text" value="" name="s" id="s" />
+					<label for="s"><?php echo __($title . ': ', 'textdomain'); ?></label>
+					<input type="text" value="<?php if (!empty($_GET['s'])) echo $_GET['s']; ?>" name="s" id="s" />
 					<input type="submit" id="searchsubmit" value="<?php echo __('Meklēt', 'textdomain'); ?>" />
 					<div class="clear"></div>
 				</div>
 				<div>
 					<div class="column">
-						<div>
-							<label class="field-title">Īpašuma veids:</label>
-							<select>
-								<?php foreach ($estateType as $type): ?>
-								<option><?php echo $type->name; ?></option>
-								<?php endforeach;?>
-							</select>
-						</div>
-						<div>
-							<label class="field-title">Platība</label>
-							<input type="number" min="0" placeholder="Min"/><label> - </label><input type="number" min="0" placeholder="Max"/>
-						</div>
+						<?php $this->createDropdown('action', __('Action', 'estate'), $actions)?>
+						<?php $this->createDropdown('estate_type', __('Type', 'estate'), $types)?>
+						<?php $this->createMinMaxField('area', __('Area', 'estate'), 0); ?>
 					</div>
 					<div class="column">
-						<div>
-							<label class="field-title">Sērija:</label>
-							<select>
-								<?php foreach ($estateSerie as $serie): ?>
-								<option><?php echo $serie->name; ?></option>
-								<?php endforeach;?>
-							</select>
-						</div>
-						<div>
-							<label class="field-title">Cena:</label>
-							<input type="number" min="0"  placeholder="Min"/><label> - </label><input type="number" min="0" placeholder="Max"/><label>EUR</label>
-						</div>
+						<?php $this->createDropdown('region', __('City/Region', 'estate'), $regions)?>
+						<?php $this->createDropdown('estate_serie', __('Serie', 'estate'), $series)?>
+						<?php $this->createMinMaxField('price', __('Price', 'estate'), 0); ?>
 					</div>
 					<div class="column">
-						<div>
-							<label class="field-title">Izstabu skaits:</label>
-							<input type="number" min="0"  placeholder="Min"/><label> - </label><input type="number" min="0" placeholder="Max"/>
-						</div>
-						<div>
-							<label class="field-title">Stāvs</label>
-							<input type="number" id="floor" min="0" max="16"  placeholder="Min"/><label> - </label><input type="number" min="0" placeholder="Max"/>
-						</div>
+						<?php $this->createDropdown('residental', __('Residental complex', 'estate'), $residentals, ($_GET['region'] == 'Riga') ? 'display: block;' : 'display: none;')?>
+						<?php $this->createMinMaxField('rooms', __('Rooms', 'estate'), 0); ?>
+						<?php $this->createMinMaxField('floor', __('Floor', 'estate'), 0); ?>
 					</div>
 				</div>
 			</div>
 		</form>
 		<?php
-		
+		echo '</div>';
 		echo $after_widget;
 	}
 	
+	/**
+	 * Create min - max field for form
+	 * 
+	 * @param string $name
+	 * @param string $title
+	 * @param int $min
+	 * @param int $max
+	 */
+	public function createMinMaxField($name, $title, $min = "", $max = "") {
+		$minVal = empty($_GET['min-' . $name]) ? '' : $_GET['min-' . $name];
+		$maxVal = empty($_GET['max-' . $name]) ? '' : $_GET['max-' . $name];
+		?>
+		
+		<div>
+			<label class="field-title"><?= $title ?></label>
+			<input type="number" step="any" id="min-<?= $name ?>" name="min-<?= $name ?>" min="<?= $min ?>" max="<?= $max ?>" value="<?= $minVal ?>" placeholder="<?= __('min', 'estate')?>" /> -
+			<input type="number" step="any" id="max-<?= $name ?>" name="max-<?= $name ?>" min="<?= $min ?>" max="<?= $max ?>" value="<?= $maxVal ?>" placeholder="<?= __('max', 'estate')?>" />
+		</div>
+		
+		<?php
+	}
+
+	public function createDropdown($name, $title, $values, $style = "") {
+		$selected = empty($_GET[$name]) ? '' : $_GET[$name];
+		?>
+		
+		<div id="<?= $name ?>" style="<?= $style ?>">
+			<label class="field-title"><?= $title ?></label>
+			<select name="<?= $name ?>">
+				<option value="" <?= empty($selected) ? 'selected' : '' ?>><?php __('Select option', 'estate') ?></option>
+				<?php foreach ($values as $value): ?>
+				<option value="<?= $value ?>" <?= ($value == $selected) ? 'selected' : '' ?>><?= $value ?></option>
+				<?php endforeach;?>
+			</select>
+		</div>
+		
+		<?php
+	}
+	
+	/**
+	 * Get values for meta key
+	 * 
+	 * @param string $key
+	 * @param string $type
+	 * @param string $status
+	 * @return array
+	 */
 	function get_meta_values( $key = '', $type = 'estate', $status = 'publish' ) {
 	
 		global $wpdb;
 	
 		if( empty( $key ) )
-			return;
+			return [];
 	
-			$r = $wpdb->get_col( $wpdb->prepare( "
-					SELECT pm.meta_value FROM {$wpdb->postmeta} pm
-					LEFT JOIN {$wpdb->posts} p ON p.ID = pm.post_id
-					WHERE pm.meta_key = '%s'
-					AND p.post_status = '%s'
-					AND p.post_type = '%s'
-					", $key, $status, $type ) );
-	
-			return array_unique($r);
+		$r = $wpdb->get_col( $wpdb->prepare( "
+				SELECT pm.meta_value FROM {$wpdb->postmeta} pm
+				LEFT JOIN {$wpdb->posts} p ON p.ID = pm.post_id
+				WHERE pm.meta_key = '%s'
+				AND p.post_status = '%s'
+				AND p.post_type = '%s'
+				", $key, $status, $type ) );
+
+		return array_unique($r);
 	}
 	
-	/* Meta keys to query
+	/**
+	 * Set search query for estates
 	 * 
-	 * $query->set(
-  'meta_query', 
-  array(
-    array(
-      'key' => 'shru_price',
-      'value' => $_GET['minPrice'],
-      'compare' => '>=',
-      'type' => 'NUMERIC'
-    )
-  )
-);
-	 * */
+	 * @param WP_Query $query
+	 */
+	public static function setSearchQuery($query) {
+		if (empty($_GET['post_type']) || $_GET['post_type'] != 'estate') {
+			return;
+		}
+		
+		$meta = [];
+		
+		if (!empty($_GET['min-price'])) {
+			$meta[] = array(
+				'key' => 'price',
+				'value' => $_GET['min-price'],
+				'compare' => '>=',
+				'type' => 'NUMERIC'
+			);
+		}
+		
+		if (!empty($_GET['max-price'])) {
+			$meta[] = array(
+				'key' => 'price',
+				'value' => $_GET['max-price'],
+				'compare' => '<=',
+				'type' => 'NUMERIC'
+			);
+		}
+		
+		if (!empty($_GET['min-area'])) {
+			$meta[] = array(
+				'key' => 'area',
+				'value' => $_GET['min-area'],
+				'compare' => '>=',
+				'type' => 'NUMERIC'
+			);
+		}
+		
+		if (!empty($_GET['max-area'])) {
+			$meta[] = array(
+				'key' => 'area',
+				'value' => $_GET['max-area'],
+				'compare' => '<=',
+				'type' => 'NUMERIC'
+			);
+		}
+		
+		$metaQuery = [];
+		
+		if (count($meta) > 1) {
+			$metaQuery = array_merge($metaQuery, ['relation' => 'AND']);
+		}
+		
+		if (!empty($meta)) {
+			$metaQuery = array_merge($metaQuery, $meta);
+			
+			error_log(print_r($metaQuery, true));
+			
+			$query->set(
+				'meta_query',
+				$metaQuery
+			);
+		}
+		
+	}
 }
--- estate.php
+++ estate.php
@@ -27,7 +27,7 @@ function addEstatePostType() {
             'rewrite'     => ['slug' => 'estates'],
             'supports'    => array( 'title', 'thumbnail', 'editor' ),
             'menu_icon'   => 'dashicons-store',
-            'taxonomies'    => array('category', 'post_tag'),
+            'taxonomies'    => array(),
             'capability_type' => 'post',
         ]
     );
@@ -83,75 +83,129 @@ function addEstatePostType() {
 	);
 	
 	register_taxonomy( 'estate_type', array( 'estate' ), $args_type );
+	
+	// Add new taxonomy - City
+	$labels_city = array(
+			'name'              => _x( 'Cities/Regions', 'taxonomy general name', 'estate' ),
+			'singular_name'     => _x( 'City/Region', 'taxonomy singular name', 'estate' ),
+			'search_items'      => __( 'Search cities/regions', 'estate' ),
+			'all_items'         => __( 'All cities/regions', 'estate' ),
+			'parent_item'       => __( 'Parent city/region', 'estate' ),
+			'parent_item_colon' => __( 'Parent city/region:', 'estate' ),
+			'edit_item'         => __( 'Edit city/region', 'estate' ),
+			'update_item'       => __( 'Update city/region', 'estate' ),
+			'add_new_item'      => __( 'Add New city/region', 'estate' ),
+			'new_item_name'     => __( 'New city/region', 'estate' ),
+			'menu_name'         => __( 'City/Region', 'estate' ),
+	);
+	
+	$args_city = array(
+			'hierarchical'      => true,
+			'labels'            => $labels_city,
+			'show_ui'           => true,
+			'show_admin_column' => true,
+			'query_var'         => true,
+			'rewrite'           => array( 'slug' => 'region' ),
+	);
+	
+	register_taxonomy( 'region', array( 'estate' ), $args_city );
+	
+	// Add new taxonomy - residental
+	$labels_residental = array(
+			'name'              => _x( 'Residental complex', 'taxonomy general name', 'estate' ),
+			'singular_name'     => _x( 'Residental complex', 'taxonomy singular name', 'estate' ),
+			'search_items'      => __( 'Search residental complex', 'estate' ),
+			'all_items'         => __( 'All residental complexes', 'estate' ),
+			'parent_item'       => __( 'Parent residental complex', 'estate' ),
+			'parent_item_colon' => __( 'Parent residental complex:', 'estate' ),
+			'edit_item'         => __( 'Edit residental complex', 'estate' ),
+			'update_item'       => __( 'Update residental complex', 'estate' ),
+			'add_new_item'      => __( 'Add New residental complex', 'estate' ),
+			'new_item_name'     => __( 'New residental complex', 'estate' ),
+			'menu_name'         => __( 'Residental complex', 'estate' ),
+	);
+	
+	$args_residental = array(
+			'hierarchical'      => true,
+			'labels'            => $labels_residental,
+			'show_ui'           => true,
+			'show_admin_column' => true,
+			'query_var'         => true,
+			'rewrite'           => array( 'slug' => 'residental' ),
+	);
+	
+	register_taxonomy( 'residental', array( 'estate' ), $args_residental );
+	
+	// Add new taxonomy - action
+	$labels_action = array(
+			'name'              => _x( 'Action', 'taxonomy general name', 'estate' ),
+			'singular_name'     => _x( 'Action', 'taxonomy singular name', 'estate' ),
+			'search_items'      => __( 'Search actions', 'estate' ),
+			'all_items'         => __( 'All actions', 'estate' ),
+			'parent_item'       => __( 'Parent action', 'estate' ),
+			'parent_item_colon' => __( 'Parent action:', 'estate' ),
+			'edit_item'         => __( 'Edit action', 'estate' ),
+			'update_item'       => __( 'Update action', 'estate' ),
+			'add_new_item'      => __( 'Add New action', 'estate' ),
+			'new_item_name'     => __( 'New action', 'estate' ),
+			'menu_name'         => __( 'Action', 'estate' ),
+	);
+	
+	$args_action = array(
+			'hierarchical'      => true,
+			'labels'            => $labels_action,
+			'show_ui'           => true,
+			'show_admin_column' => true,
+			'query_var'         => true,
+			'rewrite'           => array( 'slug' => 'action' ),
+	);
+	
+	register_taxonomy( 'action', array( 'estate' ), $args_action );
 }
 
-// register_activation_hook( __FILE__, 'addEstatePostType' );
+// Register post type
 
 add_action('init', 'addEstatePostType');
 
 // Add metadata boxes
 
 function admin_init(){
-  add_meta_box("info-meta", __("Information", 'estate'), "price", "estate", "side", "low");
-  add_meta_box("coordinates-meta", __("Coordinates", 'estate'), "coordinates", "estate", "side", "low");
   add_meta_box("address_meta", __("Location", 'estate'), "address_meta", "estate", "normal", "low");
 }
- 
-function price(){
-  global $post;
-  $custom = get_post_custom($post->ID);
-  $price = $custom["price"][0];
-  $roomCount = $custom["room_count"][0];
-  $floor = $custom["floor"][0];
-  ?>
-  	<div>
-	  <label><?php _e("Price"); ?>:</label><br>
-	  <input name="price" value="<?php echo $price; ?>" />
+
+function address_meta() {
+	?>
+	<div style="display: inline-block;">
+		<?= creatInputField('street', __('Street', 'estate')) ?>
+		<?= creatInputField('longitude', __('Longitude', 'estate'), "number") ?>
+		<?= creatInputField('latitude', __('Latitude', 'estate'), "number") ?>
 	</div>
-	<div>
-	  <label><?php _e("Room count"); ?>:</label><br>
-	  <input name="room_count" value="<?php echo $roomCount; ?>" />
-	</div>	
-	<div>
-	  <label><?php _e("Floor"); ?>:</label><br>
-	  <input name="floor" value="<?php echo $floor; ?>" />
-	</div>	
-  <?php
+	<div style="display: inline-block;">
+		<?= creatInputField('price', __('Price', 'estate'), "number") ?>
+		<?= creatInputField('rooms', __('Room count', 'estate'), "number") ?>
+		<?= creatInputField('floor', __('Floor', 'estate'), "number") ?>
+		<?= creatInputField('area', __('Area', 'estate'), "number") ?>
+	</div>
+    <?php 
 }
 
-function coordinates() {
+/**
+ * Create simple input field
+ * 
+ * @param string $name
+ * @param string $title
+ * @param string $type
+ */
+function creatInputField($name, $title, $type = "text") {
 	global $post;
-	$custom = get_post_custom($post->ID);
-	$longitude = $custom["longitude"][0];
-	$latitude = $custom["latitude"][0];
+	$value = get_post_custom($post->ID)[$name][0];
 	?>
 	<div>
-		<label><?php _e("Longitude"); ?>:</label><br/>
-		<input name="longitude" value="<?php echo $longitude; ?>"/>
-	</div>
-	<div>
-		<label><?php _e("Latitude"); ?>:</label><br/>
-		<input name="latitude" value="<?php echo $latitude; ?>" />
+		<label><?= $title ?>:</label><br/>
+		<input type="<?= $type ?>" step="any" name="<?= $name ?>" value="<?= $value ?>"/>
 	</div>
 	<?php
 }
- 
-function address_meta() {
-  global $post;
-  $custom = get_post_custom($post->ID);
-  $country = $custom["country"][0];
-  $city = $custom["city"][0];
-  $street = $custom["street"][0];
-?>
-  <p><label><?php _e("Country"); ?>:</label><br />
-  <input name="country" style="width: 100%;" value="<?php echo $country; ?>"></p>
-  <p><label><?php _e("City"); ?>:</label><br />
-  <input name="city" style="width: 100%;" value="<?php echo $city; ?>"></p>
-  <p><label><?php _e("Street"); ?>:</label><br />
-  <input name="street" style="width: 100%;" value="<?php echo $street; ?>"></p>
-  <?php
-}
-
 add_action("admin_init", "admin_init");
 
 
@@ -174,6 +228,7 @@ function save_details(){
 	update_post_meta($post->ID, "price", $_POST["price"]);
 	update_post_meta($post->ID, "serie", $_POST["serie"]);
 	update_post_meta($post->ID, "floor", $_POST["floor"]);
+	update_post_meta($post->ID, "area", $_POST["area"]);
 	update_post_meta($post->ID, "country", $_POST["country"]);
 	update_post_meta($post->ID, "city", $_POST["city"]);
 	update_post_meta($post->ID, "street", $_POST["street"]);
@@ -222,4 +277,8 @@ function register_search(){
 	register_widget("EstateSearchWidget");
 }
 
-add_action('widgets_init', "register_search");
+add_action('widgets_init', 'register_search');
+
+add_action('pre_get_posts', array('EstateSearchWidget','setSearchQuery'));
+
+
